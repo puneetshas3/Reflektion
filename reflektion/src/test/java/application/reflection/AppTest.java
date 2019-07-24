6211c6a24db6842052badf3d199e32d4324f7ed4
Shastri, Puneet <puneetshas3@gmail.com>
2019-07-25 00:11:37 +0530

    adding a project

---
+++ reflektion/src/test/java/application/reflection/AppTest.java
@@ -0,0 +1,38 @@
+package application.reflection;
+
+import static org.testng.Assert.assertTrue;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+		return null;
+        
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
